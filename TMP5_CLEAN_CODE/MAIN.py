492928591283f82a482c6aaeb31ff5e9b15be09e
Nattapat  Juthaprachakul <nattapat.juthaprachakul17@imperial.ac.uk>
2018-08-15 11:54:49 +0100

    fixed

--- TMP5_CLEAN_CODE/MAIN.py
+++ TMP5_CLEAN_CODE/MAIN.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     print("best_topology: ", best_topology)
     verify_model(best_topology,DATASET)
     '''
-    model = ['c_7','c_6','c_11','c_9']
+    model = ['c_12','c_6','c_11','c_11']
     # c_7,c_6,c_11,c_9
     DATASET = 'cifar10'
     # DATASET = 'mnist'
